9b54138ed7e33e9ea4b265c4fc2d121c4543b4a9
vk0942 <vanshkathnawal0942@gmail.com>
2023-07-25 19:30:26 +0530

    A safe commit

--- src/menu.js
+++ src/menu.js
@@ -1,25 +1,46 @@
-const inbox = document.createElement('button'); inbox.textContent = "Inbox";
-const today = document.createElement('button'); today.textContent = "Today";
-const week = document.createElement('button');  week.textContent = "This Week";
+function project(Name , Storage , Button)//factory function project
+{
+    return{
+        Name,
+        Storage,
+        Button,
+    }
+}
+let p_Array = [];
+const inbox = project('Inbox',[] , document.createElement('button')); inbox.Button.textContent = inbox.Name;
+const today = project('Today',[],document.createElement('button')); today.Button.textContent = today.Name;
+const week = project('Week',[] ,document.createElement('button')); week.Button.textContent = week.Name;
+p_Array.push(inbox);
+p_Array.push(today);
+p_Array.push(week);
+
 function Menu()
 {
     const menu = document.createElement('div');
     menu.id = "menu-content";
-    const default_projects = document.createElement('div');
-    const project_heading = document.createElement('div'); project_heading.id="project_heading";
-    const projects_added = document.createElement('div'); 
-
-
-    default_projects.appendChild(inbox);
-    default_projects.appendChild(today);
-    default_projects.appendChild(week);
-    project_heading.textContent = "Projects";
-
+    const default_projects = document.createElement('div'); default_projects.id="default_projects";
+    const project_Title = document.createElement('div'); project_Title.id="project_Title";
+    const menu_bottom = document.createElement('div'); menu_bottom.id="menu_bottom";
+    const projects_added = document.createElement('div');  projects_added.id="projects_added";
+    const Add_project = document.createElement('button'); Add_project.id="Add_project";
+    Add_project.textContent = " + Add_project ";
+    Add_project.addEventListener('click',()=>{
+        const dummy_project = project("Dummy_project",[],document.createElement('button'));
+        dummy_project.Button.textContent = dummy_project.Name;
+        projects_added.appendChild(dummy_project.Button);
+        p_Array.push(dummy_project);
+    })
+    projects_added.appendChild(Add_project);
+    default_projects.appendChild(inbox.Button);
+    default_projects.appendChild(today.Button);
+    default_projects.appendChild(week.Button);
+    project_Title.textContent = "Projects";
+    menu_bottom.appendChild(projects_added);
     menu.appendChild(default_projects);
-    menu.appendChild(project_heading);
-    menu.appendChild(projects_added);
+    menu.appendChild(project_Title);
+    menu.appendChild(menu_bottom);
     
     return menu;
 }
 export default Menu;
-export { inbox,today,week };
+export {p_Array};
--- src/tasks.js
+++ src/tasks.js
@@ -1,24 +1,29 @@
-import {inbox,today,week} from './menu.js';
+import {p_Array} from './menu.js';
+import task_ICON from './Icons/task.svg';
 
-let tasks_store = {
-    inbox:[],
-    today:[],
-    week:[],
-}
+
 function Task(title,priority)
 {
    return { title , priority };
 }
 function Fill_tasks(project_tasks,dummy_task)
 {
- 
+  
    const dummy_task_div = document.createElement('div');
    dummy_task_div.id="dummy_task_div";
+   const dummy_div_left  = document.createElement('div');
+   const task_icon = document.createElement('img');
+   task_icon.src = task_ICON; 
+   task_icon.id="task_icon";
    const task_title = document.createElement('div');
    task_title.textContent= dummy_task.title;
+   dummy_div_left.appendChild(task_icon);
+   dummy_div_left.appendChild(task_title);
+   dummy_div_left.id="dummy_div_left";
    const task_priority = document.createElement('div');
-   task_title.textContent= dummy_task.priority;
-   dummy_task_div.appendChild(task_title);
+   task_priority.textContent= dummy_task.priority;
+   
+   dummy_task_div.appendChild(dummy_div_left);
    dummy_task_div.appendChild(task_priority);
    project_tasks.appendChild(dummy_task_div);
 }  
@@ -28,51 +33,62 @@ function prefill_tasks(project_tasks,task_arr)
        Fill_tasks(project_tasks,element);
    });
 }
-function inbox_list()
+
+function project_list(project)
 {
-   const inbox_panel = document.createElement('div'); inbox_panel.id="inbox_panel";
-   const inbox_heading = document.createElement('div');
-   inbox_heading.textContent = "Inbox"; inbox_heading.id="inbox_heading";
-   const inbox_tasks = document.createElement('div');
-   inbox_tasks.id="inbox_tasks";
+   const project_panel = document.createElement('div'); project_panel.id="project_panel";
+   const project_heading = document.createElement('div');
+   project_heading.textContent = project.Name; project_heading.id="project_heading";
+   const project_tasks = document.createElement('div');
+   project_tasks.id="project_tasks";
    
 
    const Add_task = document.createElement('button');
+   Add_task.id="Add_task";
    Add_task.textContent = "+ Add task";
    Add_task.addEventListener('click',()=>{
      const dummy_task = Task("dummy_title" , "High_priority");
-     tasks_store.inbox.push(dummy_task);
-     Fill_tasks(inbox_tasks,dummy_task);
+     project.Storage.push(dummy_task);
+     Fill_tasks(project_tasks,dummy_task);
    })
-   inbox_tasks.appendChild(Add_task); 
-   prefill_tasks(inbox_tasks , tasks_store.inbox);
-   inbox_panel.appendChild(inbox_heading);
-   inbox_panel.appendChild(inbox_tasks);
-   return inbox_panel;
+   project_tasks.appendChild(Add_task); 
+   prefill_tasks(project_tasks , project.Storage);
+   project_panel.appendChild(project_heading);
+   project_panel.appendChild(project_tasks);
+   return project_panel;
 }
-function today_list()
-{
-  
-}
-function week_list()
-{
-   
-}
+
 function Tasks()
 {
     const task_list = document.createElement('div');
-    inbox.addEventListener('click',()=>{
-        console.log(tasks_store.inbox);
-        task_list.innerHTML="";
+    task_list.id="task_list";
+    p_Array.forEach(project =>{
+        console.log(project.Button);
+        project.Button.addEventListener('click',()=>{
+            console.log(p_Array);
+            task_list.innerHTML="";
+            task_list.appendChild(project_list(project));
+        })
+    })
+    // inbox.Button.addEventListener('click',()=>{
+    //     console.log(inbox.Storage);
+    //     task_list.innerHTML="";
        
-        task_list.appendChild(inbox_list());
-    })
-    today.addEventListener('click',()=>{
-        task_list.appendChild(today_list());
-    })
-    week.addEventListener('click',()=>{
-        week.appendChild(week_list());
-    })
+    //     task_list.appendChild(project_list(inbox));
+    // })
+    // today.Button.addEventListener('click',()=>{
+    //     console.log(inbox.Storage);
+    //     task_list.innerHTML="";
+       
+    //     task_list.appendChild(project_list(today));
+    // })
+    // week.Button.addEventListener('click',()=>{
+    //     console.log(inbox.Storage);
+    //     task_list.innerHTML="";
+       
+    //     task_list.appendChild(project_list(week));
+    // })
+
     return task_list;
 }
 export default Tasks;
