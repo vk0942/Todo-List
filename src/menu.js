bbd48d1ac941ff2f8de75b83e3479b85b8a7a96d
vk0942 <vanshkathnawal0942@gmail.com>
2023-07-28 00:47:42 +0530

    Implemented all the features just local storage is left gonna implement it in future

--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,6 @@
-function project(Name , Storage , Button)//factory function project
+function project(Name, Storage, Button)//factory function project
 {
-    return{
+    return {
         Name,
         Storage,
         Button,
@@ -10,56 +10,104 @@ function project(Name , Storage , Button)//factory function project
 let p_Array = [];
 
 // three default objects
-const inbox = project('Inbox',[] , document.createElement('button')); inbox.Button.textContent = inbox.Name;
-const today = project('Today',[],document.createElement('button')); today.Button.textContent = today.Name;
-const week = project('Week',[] ,document.createElement('button')); week.Button.textContent = week.Name;
+const inbox = project('Inbox', [], document.createElement('button')); inbox.Button.textContent = inbox.Name;
+const today = project('Today', [], document.createElement('button')); today.Button.textContent = today.Name;
+const week = project('Week', [], document.createElement('button')); week.Button.textContent = week.Name;
 
 p_Array.push(inbox);
 p_Array.push(today);
 p_Array.push(week);
 
 //Button that adds new projects in p_Array here i have used dummy projects for simplicity later i will add the form to add project title
-const Add_project = document.createElement('button'); Add_project.id="Add_project";
+const Add_project = document.createElement('button'); Add_project.id = "Add_project";
+
+const form = document.createElement('form');
+
+
 
 // i divided menu into three sections mainly first the default_projects that is inbox week and today
 // second section is heading Project simple
 // third section is the projects_added section which stores the newly added projects
 
-function Menu()
-{
+function Menu() {
+    function Project_form() {
+     
+        form.action = '/submit';
+        const input = document.createElement('input');
+        input.id="project_title";
+        input.type = 'text';
+        input.name = 'project_title';
+        input.maxLength = 17;
+        input.required = true;
+        input.placeholder = "Project Title";
+        const submit_btn = document.createElement('button');
+        const cancel = document.createElement('button');
+        submit_btn.id="submit_btn";
+        cancel.id="cancel";
+        cancel.type = "button";
+        submit_btn.type = 'submit';
+        submit_btn.textContent = 'Create Project';
+ 
+        cancel.textContent = 'Cancel';
+        cancel.addEventListener('click',()=>{
+            
+            form.reset();
+            form.style.display = "none";
+            Add_project.style.display = "flex";
+        })
+        form.appendChild(input);
+        form.appendChild(submit_btn);
+        form.appendChild(cancel);
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+          
+            const formData = new FormData(form);
+            const formValues = Object.fromEntries(formData.entries());
+          
+            // Now you can access the form values and store them in a variable or perform any desired actions
+            const name = formValues.project_title;
+            const dummy_project = project(name, [], document.createElement('button'));
+            dummy_project.Button.textContent = dummy_project.Name;
+            projects_added.appendChild(dummy_project.Button);
+
+            //here projects are added when Add_project button is clicked
+            p_Array.push(dummy_project);
+            form.reset();
+            form.style.display = "none";
+            Add_project.style.display = "flex";
+            console.log(p_Array);
+          });
+        return form;
+    }
+
     const menu = document.createElement('div');
     menu.id = "menu-content";
-    const default_projects = document.createElement('div'); default_projects.id="default_projects";
-    const project_Title = document.createElement('div'); project_Title.id="project_Title";
-    const menu_bottom = document.createElement('div'); menu_bottom.id="menu_bottom";
-    const projects_added = document.createElement('div');  projects_added.id="projects_added";
+    const default_projects = document.createElement('div'); default_projects.id = "default_projects";
+    const project_Title = document.createElement('div'); project_Title.id = "project_Title";
+    const menu_bottom = document.createElement('div'); menu_bottom.id = "menu_bottom";
+    const projects_added = document.createElement('div'); projects_added.id = "projects_added";
     // const Add_project = document.createElement('button'); Add_project.id="Add_project";
+    const project_form = Project_form();
+    
     Add_project.textContent = " + Add_project ";
-    Add_project.addEventListener('click',()=>{
-        const dummy_project = project("Dummy_project",[],document.createElement('button'));
-        dummy_project.Button.textContent = dummy_project.Name;
-        projects_added.appendChild(dummy_project.Button);
-        dummy_project.Button.addEventListener('click',()=>{
-             // i want this function to work same as  line 71 ans 72 in task.js which i am unable to do;
-        })
-        //here projects are added when Add_project button is clicked
-        p_Array.push(dummy_project);
+    Add_project.addEventListener('click', () => {
+        Add_project.style.display = "none";
+        project_form.style.display = "flex";
     })
-    projects_added.appendChild(Add_project);
-    p_Array.forEach(item =>{
+    menu_bottom.appendChild(Add_project);
+    menu_bottom.appendChild(project_form);
+    p_Array.forEach(item => {
         default_projects.appendChild(item.Button);
-        
+
     })
-    // default_projects.appendChild(inbox.Button);
-    // default_projects.appendChild(today.Button);
-    // default_projects.appendChild(week.Button);
+
     project_Title.textContent = "Projects";
     menu_bottom.appendChild(projects_added);
     menu.appendChild(default_projects);
     menu.appendChild(project_Title);
     menu.appendChild(menu_bottom);
-    
+
     return menu;
 }
 export default Menu;
-export {p_Array,Add_project};
+export { p_Array,form };
--- src/tasks.js
+++ src/tasks.js
@@ -1,10 +1,10 @@
-import {p_Array,Add_project} from './menu.js';
+import {p_Array,form} from './menu.js';
 import task_ICON from './Icons/task.svg';
 
 
-function Task(title,priority)
+function Task(title,Del,date)
 {
-   return { title , priority };
+   return { title ,Del,date};
 }
 //This function fills tasks in the project_tasks of the  respective  project
 function Fill_tasks(project_tasks,dummy_task)
@@ -13,20 +13,37 @@ function Fill_tasks(project_tasks,dummy_task)
    const dummy_task_div = document.createElement('div');
    dummy_task_div.id="dummy_task_div";
    const dummy_div_left  = document.createElement('div');
+   dummy_div_left.id="dummy_div_left";
+   const dummy_div_right  = document.createElement('div');
+   const task_date = document.createElement('span');
+   task_date.textContent = "Due date:- "+ dummy_task.date;
+   task_date.id="task_date";
    const task_icon = document.createElement('img');
+   const Del = dummy_task.Del;
+   Del.textContent = "Delete";
+
+   Del.id="cancel";
    task_icon.src = task_ICON; 
    task_icon.id="task_icon";
    const task_title = document.createElement('div');
    task_title.textContent= dummy_task.title;
    dummy_div_left.appendChild(task_icon);
    dummy_div_left.appendChild(task_title);
+   dummy_div_left.appendChild(task_date);
+   dummy_div_right.appendChild(Del);
    dummy_div_left.id="dummy_div_left";
-   const task_priority = document.createElement('div');
-   task_priority.textContent= dummy_task.priority;
+   dummy_div_right.id="dummy_div_right";
+//    const task_priority = document.createElement('div');
+//    task_priority.textContent= dummy_task.priority;
    
    dummy_task_div.appendChild(dummy_div_left);
-   dummy_task_div.appendChild(task_priority);
+   dummy_task_div.appendChild(dummy_div_right);
+//    dummy_task_div.appendChild(task_priority);
    project_tasks.appendChild(dummy_task_div);
+   Del.addEventListener('click',()=>{
+    project_tasks.removeChild(dummy_task_div);
+
+   })
 }  
 function prefill_tasks(project_tasks,task_arr)
 {
@@ -40,22 +57,77 @@ function prefill_tasks(project_tasks,task_arr)
 // and project_tasks is the section which actually stores the list of all the tasks in the project and it is filled with the help of Fill_tssk() function above
 function project_list(project)
 {
+    function Task_form()
+    {
+       const task_form = document.createElement('form');
+       task_form.action="/submit";
+       task_form.id="task_form";
+       const input = document.createElement('input');
+       input.id="task_title";
+       input.type = 'text';
+       input.name = 'task_title';
+       input.maxLength = 30;
+       input.required = true;
+       input.placeholder = "Task Title";
+       const date_input = document.createElement('input');
+       date_input.id="date_input";
+       date_input.type="date";
+       date_input.name = "task_date";
+       date_input.placeholder = "dd-mm-yyyy";
+       date_input.required = true;
+       const submit_btn = document.createElement('button');
+       const cancel = document.createElement('button');
+       submit_btn.id="submit_btn";
+       cancel.id="cancel";
+       cancel.type = "button";
+       cancel.textContent = "Cancel";
+       cancel.addEventListener('click',()=>{
+        task_form.reset();
+        task_form.style.display = "none";
+       })
+       submit_btn.type = 'submit';
+       submit_btn.textContent = 'Add task';
+       task_form.appendChild(input);
+       task_form.appendChild(date_input);
+       task_form.appendChild(submit_btn);
+       task_form.appendChild(cancel);
+       task_form.addEventListener('submit',function(event){
+        event.preventDefault();
+        
+        const formData = new FormData(task_form);
+        const formValues = Object.fromEntries(formData.entries());
+
+        const task_title = formValues.task_title;
+        console.log(task_title);
+        const task_date  = formValues.task_date;
+        console.log(task_date);
+        const dummy_task = Task(task_title,document.createElement('button'),task_date);
+        project.Storage.push(dummy_task);
+        dummy_task.Del.addEventListener('click',()=>{
+            const i = project.Storage.indexOf(dummy_task);
+            project.Storage.splice(i,1);
+        })
+        Fill_tasks(project_tasks,dummy_task);
+        task_form.reset();
+        task_form.style.display = "none";
+       })
+       return task_form;
+    }
    const project_panel = document.createElement('div'); project_panel.id="project_panel";
    const project_heading = document.createElement('div');
    project_heading.textContent = project.Name; project_heading.id="project_heading";
    const project_tasks = document.createElement('div');
    project_tasks.id="project_tasks";
-   
+   const task_form = Task_form();
 
    const Add_task = document.createElement('button');
    Add_task.id="Add_task";
    Add_task.textContent = "+ Add task";
    Add_task.addEventListener('click',()=>{
-     const dummy_task = Task("dummy_title" , "High_priority");
-     project.Storage.push(dummy_task);
-     Fill_tasks(project_tasks,dummy_task);
+      task_form.style.display = "flex";
    })
    project_tasks.appendChild(Add_task); 
+   project_tasks.appendChild(task_form);
    prefill_tasks(project_tasks , project.Storage);
    project_panel.appendChild(project_heading);
    project_panel.appendChild(project_tasks);
@@ -76,14 +148,15 @@ function Tasks()
         })
     })
     // The problem lies here this thing runs now when i have not added any input form that takes project title and creates and oblect and pushes it into the p_Array array
-    // Add_project.addEventListener('click',()=>{
-    //         const project = p_Array[p_Array.length-1];
-    //         project.Button.addEventListener('click',()=>{
+    form.addEventListener('submit',function(event){
+            event.preventDefault();
+            const project = p_Array[p_Array.length-1];
+            project.Button.addEventListener('click',()=>{
                 
-    //             task_list.innerHTML="";
-    //             task_list.appendChild(project_list(project));
-    //         })
-    //     })
+                task_list.innerHTML="";
+                task_list.appendChild(project_list(project));
+            })
+    })
     
 
 
